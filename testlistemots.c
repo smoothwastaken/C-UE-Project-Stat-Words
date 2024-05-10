39785e02f0875f087be799a767428edc08148c23
smoothwastaken <clery.arque-ferradou@univ-tlse3.fr>
2024-05-10 18:22:20 +0200

    [test] added test content

--- testlistemots.c
+++ testlistemots.c
@@ -4,5 +4,19 @@
 
 int main(void) {
 
+    // Nouvelle liste
+    tListeMots liste = creerListeMots();
+
+    // Ajout de mots
+    ajouterMotAvecOcc(liste, "Bonjour", 1);
+    ajouterMotAvecOcc(liste, "Salut", 2);
+    ajouterMotAvecOcc(liste, "Hello", 1);
+
+    // Affichage de la liste
+    ecrireListeMots(liste, stdout);
+
+    // Destruction de la liste
+    detruireListeMots(liste);
+
     return EXIT_SUCCESS;
 }
