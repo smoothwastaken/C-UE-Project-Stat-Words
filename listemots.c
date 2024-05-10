ef94d99c843bf954621c65e9fa98d6d7f425b7dc
smoothwastaken <clery.arque-ferradou@univ-tlse3.fr>
2024-05-10 18:20:31 +0200

    [new] new public functions for listemots.c

--- listemots.c
+++ listemots.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "listemots.h"
 
 #define TAILLE_MAX 50
 
@@ -54,4 +55,89 @@ struct sMot * motCreer(char *mot, int occ) {
 void motLiberer(struct sMot *mot) {
     free(mot -> mot);
     free(mot);
+}
+
+tListeMots creerListeMots(void) {
+    // Alloue la structure de la liste
+    tListeMots liste = malloc(sizeof(struct sListeMots));
+    if (liste == NULL) {
+        return NULL;
+    }
+
+    // Initialise les valeurs de la liste
+    liste -> debut = NULL;
+    liste -> fin = NULL;
+    liste -> nbMots = 0;
+
+    return liste;
+}
+
+void detruireListeMots(tListeMots liste) {
+    // Libère chaque mot de la liste
+    struct sMot * courant = liste -> debut;
+    struct sMot * suivant = NULL;
+    while (courant != NULL) {
+        suivant = courant -> suivant;
+        motLiberer(courant);
+        courant = suivant;
+    }
+
+    // Libère la liste
+    free(liste);
+
+    return;
+}
+
+int nombreDeMots(tListeMots liste) {
+    // Retourne le nombre de mots de la liste
+    return liste -> nbMots;
+}
+
+void ajouterMotAvecOcc(tListeMots liste, char *mot, int nbOcc) {
+    // Crée un nouveau mot
+    struct sMot * nouveauMot = motCreer(mot, nbOcc);
+
+    // Vérifie si le mot a été créé
+    if (nouveauMot == NULL) {
+        return;
+    }
+
+    // Ajoute le mot à la liste selon si la liste est vide ou non
+    // Si la liste est vide, le mot est le premier et le dernier de la liste.
+    if (liste -> debut == NULL) {
+        liste -> debut = nouveauMot;
+        liste -> fin = nouveauMot;
+    // Sinon, le mot est ajouté à la fin de la liste.
+    } else {
+        liste -> fin -> suivant = nouveauMot;
+        liste -> fin = nouveauMot;
+    }
+
+    // Incrémente le nombre de mots de la liste
+    liste -> nbMots ++;
+
+    return;
+}
+
+void ajouterMot(tListeMots liste, char *mot) {
+    // Ajoute un mot à la liste avec une occurrence de 1
+    ajouterMotAvecOcc(liste, mot, 1);
+}
+
+void ecrireListeMots(tListeMots liste, FILE *fich) {
+    // Écrit les mots de la liste dans un fichier
+    struct sMot * courant = liste -> debut;
+
+    // Écrit chaque mot de la liste dans le fichier
+    while (courant != NULL) {
+        // Écrit le mot et un espace
+        fprintf(fich, "%s ", courant -> mot);
+        // Passe au mot suivant
+        courant = courant -> suivant;
+    }
+
+    // Retour à la ligne
+    fprintf(fich, "\n");
+
+    return;
 }
--- listemots.h
+++ listemots.h
@@ -1,9 +1,21 @@
+#include <stdio.h>
+
 #ifndef LISTEMOTS_H
 #define LISTEMOTS_H
 
 // Type permettant de manipuler une liste de mots
 typedef struct sListeMots * tListeMots ;
 
+extern tListeMots creerListeMots(void);
 
+extern void detruireListeMots(tListeMots liste);
+
+extern int nombreDeMots(tListeMots liste);
+
+extern void ajouterMotAvecOcc(tListeMots liste, char *mot, int nbOcc);
+
+extern void ajouterMot(tListeMots liste, char *mot);
+
+extern void ecrireListeMots(tListeMots liste, FILE *fich);
 
 #endif /* LISTEMOTS_H */
